3dbe987c09af897ada451fa276f6ce92b1379f16
Дарья Шипилова <sh.dari@list.ru>
2022-10-16 21:01:44 +0300

    task-method-context

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -8,8 +8,16 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    subscribe() {
+        EventEmitter.on('click', callback);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', callback);
+    },
+};
+
+const callback = () => {
+    obj.count++;
 };
 
 /*
@@ -19,7 +27,13 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        let result = [];
+        for (let i = 0; i < args.length; i++) {
+            result.unshift(args[i]);
+        }
+        this.second(...result);
+    },
     second() {
         // здесь ничего писать не нужно
     },
